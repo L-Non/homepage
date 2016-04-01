dc5c266a245ed695253f29e3caa5a629c49a3a61
Ilja <iljapanic@gmail.com>
2016-04-01 13:16:40 +0700

    placeholder version

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+(function ($, root, undefined) {
+
+  $(function () {
+
+    'use strict';
+
+    $('.logo').realshadow({
+      type: 'text',
+      length: 4,
+      color: '119,119,119'
+    });
+
+    $('.headline').realshadow({
+      type: 'text',
+      inverse: true,
+      length: 9
+    });
+
+    $('.contact').realshadow({
+      type: 'text'
+    });
+
+
+
+  });
+
+})(jQuery, this);
+
+
 
