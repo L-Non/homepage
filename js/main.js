c0ccf27408a259ea36d3a5abe0cb7c621abe6ee2
Ilja <iljapanic@gmail.com>
2016-04-18 15:09:14 +0700

    add real shadow effect only on non-touch devices

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
 (function ($, root, undefined) {
 
-  $(function () {
+  if (Modernizr.touch) {
+    // do nothing
+  } else {
+    // add realshadow
+    $(function () {
 
-    'use strict';
+      'use strict';
 
-    $('.logo').realshadow({
-      type: 'text',
-      length: 7,
-      color: '0,0,0',
+      $('.logo').realshadow({
+        type: 'text',
+        length: 7,
+        color: '0,0,0',
+      });
+
+      $('.headline li').realshadow({
+        type: 'text'
+      });
+
+      $('.contact .btn').realshadow({
+        // type: 'text'
+        length: 1,
+        color: '102,102,102',
+        inverse: true
+      });
+
     });
-
-    $('.headline li').realshadow({
-      type: 'text'
-    });
-
-    $('.contact .btn').realshadow({
-      // type: 'text'
-      length: 1,
-      color: '102,102,102',
-      inverse: true
-    });
-
-
-
-  });
+  }
 
 })(jQuery, this);
 
